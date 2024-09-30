92fa4e0dc1c2ac8b96f30d31156307c6171d74bd
twest67 <63468489+Marzipayn@users.noreply.github.com>
2024-09-30 13:47:52 -0400

    Setup project activities and added headings to main feature screens
    - created new project folder "sprint1-main"
    - added activities "main" (splash screen), "login", "home", "accomodations", "destination", "dining", "logistics", "transportation", and "travel community" with java files and corresponding xmls
    - created headers for each main feature screen through textviews

---
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.sprint1_main.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+import com.example.sprint1_main.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate called");
+    }
+
+
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+}
---
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/viewmodel/MainViewModel.java
@@ -0,0 +1,4 @@
+package com.example.sprint1_main.viewmodel;
+
+public class MainViewModel {
+}
