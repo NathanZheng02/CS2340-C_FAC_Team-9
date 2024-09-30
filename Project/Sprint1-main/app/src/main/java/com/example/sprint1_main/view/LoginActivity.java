4f61d3dadc84b63e883188c2c8ebd0eaf154ba44
Dat Tien Mai <maitiendat7701@gmail.com>
2024-09-30 17:58:38 -0400

    Created login interface, including fields and buttons for username, password, login, and register button. Added code to check for empty username and passwords.
    Added button on main activity to navigate to login activity.

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/LoginActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/LoginActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -17,10 +20,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText usernameInput = findViewById(R.id.usernameInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+
+                if (username.isEmpty() || username == null) {
+                    usernameInput.setError("Please enter a username");
+                    return;
+                }
+                if (password.isEmpty() || password == null) {
+                    passwordInput.setError("Please enter a password");
+                    return;
+                }
+            }
+        });
         Log.d(TAG, "onCreate called");
     }
-
-
 
 
     @Override
--- Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/MainActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import com.example.sprint1_main.R;
 
@@ -15,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.goToLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
