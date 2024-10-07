ba5b73fa4861ebb51c5e86e3da110751d6edf457
Dat Tien Mai <maitiendat7701@gmail.com>
2024-10-06 21:34:33 -0400

    Checkstyle for junits

--- Project/Sprint1-main/app/src/test/java/com/example/sprint1_main/ExampleUnitTest.java
+++ Project/Sprint1-main/app/src/test/java/com/example/sprint1_main/ExampleUnitTest.java
@@ -13,20 +13,22 @@ import com.example.sprint1_main.model.UserModel;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void additionIsCorrect() {
         assertEquals(4, 2 + 2);
     }
 
     @Test
     public void userModelAccess() {
         //First User Test with getters
-        UserModel newUser = new UserModel("UserName@email.com", "123-456-7890", "UserName", 18, "goodUserName", "goodPassword");
+        UserModel newUser = new UserModel("UserName@email.com", "123-456-7890",
+                                    "UserName", 18, "goodUserName", "goodPassword");
 
         assertEquals(newUser.getName(), "UserName");
         assertEquals(newUser.getEmail(), "UserName@email.com");
 
         //Second User Test with setters
-        UserModel jonDoe = new UserModel("johnDoe@email.com", "123-456-7890", "John Doe", 25, "JohnDoe14", "secretAccountPass14!");
+        UserModel jonDoe = new UserModel("johnDoe@email.com", "123-456-7890",
+                                    "John Doe", 25, "JohnDoe14", "secretAccountPass14!");
         jonDoe.setName("Jon Doe");
         jonDoe.setEmail("jonDoe@gmail.com");
         jonDoe.setUsername("JonDoe12");
