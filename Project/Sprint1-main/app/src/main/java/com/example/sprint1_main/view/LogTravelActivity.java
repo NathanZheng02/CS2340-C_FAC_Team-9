10ca5c8e1a6e388f023898e7501ea8b5f111a138
Dat Tien Mai <maitiendat7701@gmail.com>
2024-10-24 22:14:10 -0400

    Added implementation of destination database

--- Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/LogTravelActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprint1_main/view/LogTravelActivity.java
@@ -10,104 +10,132 @@ import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.sprint1_main.R;
+import com.example.sprint1_main.model.UserModel;
+import com.example.sprint1_main.model.DestinationModel;
+import com.example.sprint1_main.view.LoginActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class LogTravelActivity extends AppCompatActivity{
-
-
-        private static final String TAG = "LogTravelActivity";
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_destination);
-
-            ImageButton logistics = findViewById(R.id.button_logistics);
-            ImageButton accommodations = findViewById(R.id.button_accommodations);
-            ImageButton dining = findViewById(R.id.button_diningEstablishments);
-            ImageButton community = findViewById(R.id.button_travelCommunity);
-            ImageButton home = findViewById(R.id.button_home);
-            Button logTravel = findViewById(R.id.button_logTravel);
-            Button calcVacation = findViewById(R.id.button_vacationTime);
-            logistics.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(LogTravelActivity.this, LogisticsActivity.class);
-                    startActivity(intent);
-                }
-            });
-            accommodations.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LogTravelActivity.this, AccomodationsActivity.class);
-                    startActivity(intent);
-                }
-            });
-            dining.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LogTravelActivity.this, DiningActivity.class);
-                    startActivity(intent);
-                }
-            });
-            community.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LogTravelActivity.this, TravelCommunityActivity.class);
-                    startActivity(intent);
-                }
-            });
-            home.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LogTravelActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                }
-            });
-            logTravel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LogTravelActivity.this, DiningActivity.class);
-                    startActivity(intent);
-                }
-            });
-            Log.d(TAG, "onCreate called");
-
-        }
+public class LogTravelActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "LogTravelActivity";
 
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
-        @Override
-        protected void onStart() {
-            super.onStart();
-            Log.d(TAG, "onStart called");
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logtravel);
 
-        @Override
-        protected void onResume() {
-            super.onResume();
-            Log.d(TAG, "onResume called");
-        }
+        ImageButton logistics = findViewById(R.id.button_logistics);
+        ImageButton accommodations = findViewById(R.id.button_accommodations);
+        ImageButton dining = findViewById(R.id.button_diningEstablishments);
+        ImageButton community = findViewById(R.id.button_travelCommunity);
+        ImageButton home = findViewById(R.id.button_home);
+        Button logTravelButton = findViewById(R.id.button_logTravel);
+        Button calcVacation = findViewById(R.id.button_vacationTime);
+        Button submit = findViewById(R.id.submitButton);
+        EditText destination = findViewById(R.id.travellocation);
+        EditText startDateField = findViewById(R.id.startDate);
+        EditText endDateField = findViewById(R.id.endDate);
+        logistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogTravelActivity.this, LogisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+        accommodations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogTravelActivity.this, AccomodationsActivity.class);
+                startActivity(intent);
+            }
+        });
+        dining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogTravelActivity.this, DiningActivity.class);
+                startActivity(intent);
+            }
+        });
+        community.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogTravelActivity.this, TravelCommunityActivity.class);
+                startActivity(intent);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogTravelActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        logTravelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogTravelActivity.this, LogTravelActivity.class);
+                startActivity(intent);
+            }
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("destinations");
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            Log.d(TAG, "onPause called");
-        }
+                String destinationName = destination.getText().toString();
+                String startDate = startDateField.getText().toString();
+                String endDate = endDateField.getText().toString();
 
-        @Override
-        protected void onStop() {
-            super.onStop();
-            Log.d(TAG, "onStop called");
-        }
+                DestinationModel destination = new DestinationModel(destinationName, startDate, endDate);
 
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-            Log.d(TAG, "onDestroy called");
-        }
+                reference.child(destinationName).setValue(destination);
+            }
+        });
+        Log.d(TAG, "onCreate called");
+
     }
+
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+}
 
